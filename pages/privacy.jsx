07e373549ab1d6305744d43727a829c4eb2bcf6e
GalaxyXKedi <117038272+GalaxyXKedi@users.noreply.github.com>
2023-01-21 22:24:00 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,7 +2,7 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Astra</h1>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for GalaxyKedy</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
         <p>⊳ Not GalaxyKedy Botu Sunucunuza Eklendiğininde Tüm İzinler Onun Elinde Olur Sahip Olduğu İzinle İse Şunlardır. <a className="text-white">(Yönetici, Sunucuyu Yönet, Rolleri Yönet, Kanalları Yönet, Kanalları Yönet, Üyeleri At/Engelle, Davet Oluştur, Kullanıcı Adlarını Yönet/Değiştir, Emoji Ve Çıkartmaları Yönet, Webhookları Yönet, Denetim Kaydını Görüntüle, Mesajları Oku/Gönder/Yönet, Metin Okuma Mesajı Gönder, Bağlantı Yerleştir, Dosya Ekle, Mesaj Geçmişini Oku, everyone/here gönder, Tepki Ekle, Harici Emoji Kullan, Bağlan/Konuş, Üyeleri Sustur/Sağırlaştır/Taşı, Ses Eylemini Kullan, Öncelikli Konuşma)</a></p>

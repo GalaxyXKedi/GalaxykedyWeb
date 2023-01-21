c5d0d74ee3ecc066da27607d4646c83fdc68145d
GalaxyXKedi <117038272+GalaxyXKedi@users.noreply.github.com>
2023-01-21 22:11:31 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -7,15 +7,15 @@ export default function Privacy() {
   <div className="animateHeader text-white text-opacity-40 mt-5">
         <p>⊳ Not GalaxyKedy Botu Sunucunuza Eklendiğininde Tüm İzinler Onun Elinde Olur Sahip Olduğu İzinle İse Şunlardır. <a className="text-white">(Yönetici, Sunucuyu Yönet, Rolleri Yönet, Kanalları Yönet, Kanalları Yönet, Üyeleri At/Engelle, Davet Oluştur, Kullanıcı Adlarını Yönet/Değiştir, Emoji Ve Çıkartmaları Yönet, Webhookları Yönet, Denetim Kaydını Görüntüle, Mesajları Oku/Gönder/Yönet, Metin Okuma Mesajı Gönder, Bağlantı Yerleştir, Dosya Ekle, Mesaj Geçmişini Oku, everyone/here gönder, Tepki Ekle, Harici Emoji Kullan, Bağlan/Konuş, Üyeleri Sustur/Sağırlaştır/Taşı, Ses Eylemini Kullan, Öncelikli Konuşma)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ Bota Oy Vermek İçin <a className="text-white" href="https://galaxykedy.vercel.app/vote">GalaxyKedy</a> Galaxykedy Yazan Beyaz Yere Tıklayınız.</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the astra bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Her kullanıcı GalaxyKedy botu zarar verme amacı taşımadığı sürece istediği gibi kullanabilir..</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using Astra.</p>   
+        <p>⊳ Galaxykedy Botu Kullanırken Belirli Komutları Aynı Anda Kullanamazsınız..</p>   
         <div className="py-3"></div>
-        <p>⊳ Astra bot does not save any data based on users.</p>
+        <p>⊳ GalaxyKedy Bot Kullanıcılara Dayalı Herhangi Bir Veri Kaydetmez.<a className="text-white">Sadece Hack Saldırılarını Tespit Etmek Amacıyla Ayda Bir Sunucu Dolaşımı Yapılır Fakat Verileriniz Umursanmaz</a></p>
         <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+        <p>⊳ Bota Veya Siteye Herhangi Bir Hack Saldırısı Yaparsanız İpiniz Bulunur Ve Gerekli İşlemler Yapılır Verileriniz Kaydedilmiyor Ama Herhangi Bir Yavaşlamada Gönderilen Paketler Kaydediliyor Oradan İpniz Bulunur Ve Yapılacak İşlemler Yapılır</p> 
     
         </div>
             </div>

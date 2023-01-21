c495b9748b3745fcb4222545b5f55edaeca3ddfd
GalaxyXKedi <117038272+GalaxyXKedi@users.noreply.github.com>
2023-01-21 22:46:48 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -27,7 +27,7 @@ export default function Index() {
               </a>
             </Link>
   <div className="py-10"></div>
-  <Link href={"https://bit.ly/3LYzaDe"}>
+  <Link href={"https://discord.com/oauth2/authorize?client_id=1026857529545855047&scope=bot%20applications.commands&permissions=2146958847"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Bota Oy Ver</a>
   </Link>

be603e72a38203e0ce99483f5372e25169bfd918
GalaxyXKedi <117038272+GalaxyXKedi@users.noreply.github.com>
2023-01-21 22:04:39 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,7 +5,7 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Astra</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the Astra bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ Not GalaxyKedy Botu Sunucunuza Eklendiğininde Tüm İzinler Onun Elinde Olur Sahip Olduğu İzinle İse Şunlardır. <a className="text-white">(Yönetici, Sunucuyu Yönet, Rolleri Yönet, Kanalları Yönet, Kanalları Yönet, Üyeleri At/Engelle, Davet Oluştur, Kullanıcı Adlarını Yönet/Değiştir, Emoji Ve Çıkartmaları Yönet, Webhookları Yönet, Denetim Kaydını Görüntüle, Mesajları Oku/Gönder/Yönet, Metin Okuma Mesajı Gönder, Bağlantı Yerleştir, Dosya Ekle, Mesaj Geçmişini Oku, everyone/here gönder, Tepki Ekle, Harici Emoji Kullan, Bağlan/Konuş, Üyeleri Sustur/Sağırlaştır/Taşı, Ses Eylemini Kullan, Öncelikli Konuşma)</a></p>
         <div className="py-3"></div>
         <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot when using certain commands from the bot commands.</p>
         <div className="py-3"></div>
